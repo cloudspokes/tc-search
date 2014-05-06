06a7bee5dd4317bf9c62732dade7d06781670b0c
Neil Hastings <neil.hastings@gmail.com>
2014-05-06 07:45:44 -0400

    Add content_type :json so we can get json headers

--- app.rb
+++ app.rb
@@ -7,16 +7,19 @@ get '/' do
 end
 
 get '/challenges/search' do
+  content_type :json
   results = HTTParty.get("#{ENV['BONSAI_URL']}/#{ENV['INDEX_CHALLENGES']}/_search?q=#{params[:q]}")
   results['hits']['hits'].to_json
 end 
 
 get '/challenges/development/search' do
+	content_type :json 
   results = HTTParty.get("#{ENV['BONSAI_URL']}/#{ENV['INDEX_CHALLENGES']}/development/_search?q=#{params[:q]}")
   results['hits']['hits'].to_json
 end 
 
 get '/challenges/design/search' do
+  content_type :json
   results = HTTParty.get("#{ENV['BONSAI_URL']}/#{ENV['INDEX_CHALLENGES']}/design/_search?q=#{params[:q]}")
   results['hits']['hits'].to_json
 end 
