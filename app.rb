0fbc21fba4d5aa7f78aee7b2ccf8a0a48a222eee
Jeff Douglas <jeff@jeffdouglas.com>
2014-05-08 17:14:18 -0400

    added cors gem

--- app.rb
+++ app.rb
@@ -2,24 +2,23 @@ require 'sinatra'
 require 'json'
 require 'httparty'
 
+enable cross_origin
+
 get '/' do
   erb :index
 end
 
 get '/challenges/search' do
-  response['Access-Control-Allow-Origin'] = '*'
   results = HTTParty.get("#{ENV['BONSAI_URL']}/#{ENV['INDEX_CHALLENGES']}/_search?q=#{params[:q]}")
   results['hits']['hits'].to_json
 end 
 
 get '/challenges/development/search' do
-  response['Access-Control-Allow-Origin'] = '*'
   results = HTTParty.get("#{ENV['BONSAI_URL']}/#{ENV['INDEX_CHALLENGES']}/development/_search?q=#{params[:q]}")
   results['hits']['hits'].to_json
 end 
 
 get '/challenges/design/search' do
-  response['Access-Control-Allow-Origin'] = '*'
   results = HTTParty.get("#{ENV['BONSAI_URL']}/#{ENV['INDEX_CHALLENGES']}/design/_search?q=#{params[:q]}")
   results['hits']['hits'].to_json
 end 
